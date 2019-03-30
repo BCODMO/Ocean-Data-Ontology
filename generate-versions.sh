4a33f7ca7c673e5e4a314292f449a3d6b4d814c1
Adam Shepherd <ashepherd@whoi.edu>
2019-03-30 10:17:35 -0400

    adds rdfs:langString; fixes DatasetMonitoredProperty rdfs:label

--- generate-versions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-URL=$1
-
-wget -O odo.jsonld "https://rdf-translator.appspot.com/convert/xml/json-ld/$1"
-wget -O odo.n3 "https://rdf-translator.appspot.com/convert/xml/n3/$1"
-wget -O odo.nt "https://rdf-translator.appspot.com/convert/xml/nt/$1"
